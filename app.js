d84d89b7ed38612e040e27e631538544d2d30dbc
matin <admatinm@gmail.com>
2022-11-02 13:30:33 +0600

    resolve cors problem

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ const expenseRoutes = require('./Routes/expenseRoutes');
 const orderRouter = require('./Routes/orderRoutes');
 const errorHandler = require('./Controller/errorController');
 //Middleware
-app.use(cors());
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
